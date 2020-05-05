4476b5c6fff085f00ac43f1fe7ae8978b3f9ab88
Kevin GOUOMBA MANGA <kevin.gouombamanga@epfedu.fr>
2020-05-05 23:34:03 +0200

    With the graph above we can see that the number of deaths is proportional to the number of confirmed cases, this also leaves us with indicators of the fragility of some regions, which by presenting fewer confirmed cases than others manage to have more deaths. This may be due to a lack of respect for security regulations, or perhaps a more fragile population, or perhaps that many confirmed cases do not bother to go to hospital.

--- model.py
+++ model.py
@@ -20,3 +20,11 @@ datasort = data.sort_values('Deaths', ascending=False)
 # Preview all of the loaded data
 print(datasort[:50])
 datasort = datasort.head(15)
+
+
+A = ["Confirmed", "Deaths"]
+datasort[A].plot.bar(figsize=(30,5), rot=0, width=0.9)
+plt.xticks(rotation=90)
+plt.show()
+
+
